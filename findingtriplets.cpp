b0b5bd21db0c587eb5ebd2ff5651454cbc299fa9
abidurrahman14004 <abidurrahman14004@gmail.com>
2023-12-29 19:49:01 +0600

    Updated triplet follows O(N^2)

--- findingtriplets.cpp
+++ findingtriplets.cpp
@@ -8,14 +8,18 @@ vector<int>v;
 for(int i=0;i<n;i++) {int x; cin >>x; v.push_back(x);}
 
 for(int i=0;i<n;i++){
-    for(int j=i+1;j<n;j++){
-        for(int k=j+1;k<n;k++){
-            if(v[i]+v[j]+v[k]==0){
-                
-                cout<<v[i]<<" "<<v[j]<<" "<<v[k]<<endl;
-            }
-        }
-    }
+
+unordered_set<int>s;
+for(int j=i+1;j<n;j++){
+    int x= -(v[i]+v[j]);
+    
+if(s.find(x)!=s.end()){
+    cout<<v[i]<<" "<<v[j]<<" "<<x<<endl;
+}
+else s.insert(v[j]);
+
+}
+
 }
 
 
